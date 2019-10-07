5ba237030f54554c6d7ef185520cce67c73e08de
Micah Levine <micah.levine@outlook.com>
2019-10-07 00:31:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,21 +8,17 @@ class Scraper
 
   def get_page
 doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
+end
 
 #doc.css(".post").each do |post|
       #course = Course.new
       #course.title = post.css("h2").text
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
-    end
+
+  def get_courses
+    self.get_page.css("post")
   end
-end
-
-
-
-
-Scraper.new.get_page
 
 
 def make_courses
@@ -44,3 +40,5 @@ end
       end
     end
   end
+
+end
